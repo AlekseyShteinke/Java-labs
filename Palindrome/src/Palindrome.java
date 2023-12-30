63750537bd8aaa33c87cf7aafb464d60c154f791
AlekseyShteinke <43885149+AlekseyShteinke@users.noreply.github.com>
2023-12-30 23:12:23 +0300

    Java lab 1

---
+++ Palindrome/src/Palindrome.java
@@ -0,0 +1,21 @@
+
+public class Palindrome {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++){
+            String s = args[i];
+            System.out.println(s + " " + isPalindrome(s));
+        }
+    }
+
+    public static String reverseString(String s){
+        String newString = "";
+        for (int i = s.length()-1; i >=0; i--){
+            newString += s.charAt(i);
+        }
+        return newString;
+    }
+
+    public static boolean isPalindrome(String s){
+        return s.equals(reverseString(s));
+    }
+}
